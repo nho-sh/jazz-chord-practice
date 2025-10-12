34f2a0c9b484703a41e55c1f492c9332edb2a616
nhosh <sulejmani.niels@gmail.com>
2025-10-12 19:19:09 +0200

    Added two songs

--- src/chord_lib.js
+++ src/chord_lib.js
@@ -26,6 +26,7 @@ const ChordLib = {
 		"A7 Ab7 Bb-7 Bb7 C7 C7b9 D- D-6 D-7 D-maj7 D7 Dbmaj7 Eb-7 Eb7 Fmaj7 G- G-6 G-7 G-maj7 Gb7",
 	misty: "A-7 Ab-7 Abmaj7 Bb-7 Bb7 C-7 C7 C7b9 D7 Db7 Eb6 Eb7 Eb7b9 Ebmaj7 F-7 F7 G-7 G-7b5",
 	moanin: "Ab9 B9 Bb Bb-9 C7 C7#9 Cm7b5 F F7b9 G-7 G7b9 Gm7b5",
+	softy_as_a_morning_sunshine: "Bb7 C7 Cm7 D7 Dm7b5 Ebmaj7 Fm7 G7",
 	saint_james_infirmary: "A7 B Bb7 Bm7 C C# Cdim Dm E Em7b5 F G",
 	st_thomas: "A7 Bb7 C C7 D-7 Dm7b5 E-7 Em7b5 F F#° G+7 G7",
 	straight_no_chaser: "Am7 Bb7 C7 D7 F7 G-7",
@@ -34,6 +35,7 @@ const ChordLib = {
 	take_the_a_train: "C D-7 D7 D7b5 F G7 G7b9",
 	the_girl_from_ipanema:
 		"A-7 B7 C7b9 D7 D7b9 Eb7 F#-7 Fmaj7 G-7 G7 Gb7 Gbmaj7 Gmaj7",
+	waltz_for_debby: "A7 Ab7 Ab°7 Amaj7 Amin7 Bb6 Bbmaj7 Bmin7 C#min7 C7 D7 Dm7 E7 Eb7 F6 F7 Fmaj7 G7 Gb7 Gm7",
 	wave: "A A-7 A7 A7b9 Ab B-7 B7b9 B9 Bb Bb9 Bb°7 C9 D-7 D7b9 Dmaj7 E E7 Ebmaj7 F#+7 F#13 F-7 Fmaj7 G G-6 G-7 G13 Gmaj7",
 	yardbird_suite: "A7 B7b9 Bb7 C6 C7 D-7 D7 E-7 E°7 F#°7 F-7 G7",
 };
